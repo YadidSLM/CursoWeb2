ef941e738c76224f04feac08e4b8461b46354fff
YadidSLM <yadid_sebastian@comunidad.unam.mx>
2025-06-17 11:49:00 -0600

    Añadir elementos html, alerts, cambiar css con javascript

--- Actividades_clase/anatomia_css_html/dynamics/index.js
+++ Actividades_clase/anatomia_css_html/dynamics/index.js
@@ -2,12 +2,21 @@
 // console.log(header);
 //header.setAttribute("style", "background-color: rgb(40, 101, 14)");
 
-const art = document.getElementById("art");
+// const art = document.getElementById("art");
+const art = document.getElementsByClassName("art");
+const aside = document.getElementById("aside");
 console.log(art);
-art.addEventListener("mouseover", () => {
-    art.style.display = "none";
+//[...art] convierte art un objeto del tipo HTMLCollection en un objeto tipo arreglo patra así usar el foreach y recorrer el nuevo arreglo
+[...art].forEach(element => {
+        element.addEventListener("mouseover", () => {
+            element.style.backgroundColor = "rgb(23, 126, 223)";
+        });
+        element.addEventListener("mouseout", ()=>{
+            element.style.backgroundColor = "rgb(110, 9, 204)";
+        });
 });
 
-window.addEventListener("blur", () => {
-    // alert("Te fuistee");
-});
+
+// window.addEventListener("blur", () => {
+//     alert("Te fuistee");
+// });
