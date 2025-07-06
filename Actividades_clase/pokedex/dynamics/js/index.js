21722fbae54d916fd3cf3ce6b6304879ec0a4c5f
YadidSLM <yadid_sebastian@comunidad.unam.mx>
2025-07-05 23:54:34 -0600

    Se añadió el fetch en js y la conexión a la base en php

--- Actividades_clase/pokedex/dynamics/js/index.js
+++ Actividades_clase/pokedex/dynamics/js/index.js
@@ -1,6 +1,7 @@
 const add = document.getElementById("agregar");
 const form = document.getElementById("form")
 const enviar = document.getElementById("enviar");
+const select_tipos = document.getElementById("select-tipos");
 
 add.addEventListener("click", ()=>{
     form.style.display = "block";
@@ -9,3 +10,17 @@ add.addEventListener("click", ()=>{
 enviar.addEventListener("click", ()=>{
     form.style.display = "none";
 });
+
+fetch("dynamics/php/tipos.php") //Toma solo en cuenta ruta absoluta, es decir, empieza a considerar las primeras carpetas o archivos que se encuentran dentro del proyecto.
+    .then((response)=>{ //.then devuelve una promesa, ¿response es lo que regresa .then? Veo que cuando se convuerte a json, regresa una promesa en json.
+        return response.json();
+    })
+    .then((datosJSON)=>{
+        console.log(datosJSON);
+        for(tipo of datosJSON){
+            select_tipos.innerHTML += "<option value='" + tipo["type_id"]+ "'>" + tipo["type_name"] + "</option>";
+        }
+    })
+    .catch((erro)=>{
+        console.log("Hubo un error: \n" + erro);
+    });
--- Actividades_clase/pokedex/dynamics/php/config.php
+++ Actividades_clase/pokedex/dynamics/php/config.php
@@ -1,7 +1,17 @@
 <?php
+
 define("DBHOST", "localhost");
 define("DBUSER", "root");
 define("PASSWORD", "");
 define("DB", "pokemon");
+
+function connect(){
+    $conexion = mysqli_connect(DBHOST, DBUSER, PASSWORD, DB);
+    if(!$conexion){
+        mysqli_error();
+        echo "No se pudo conectar a la base de datos";
+    }
+    return $conexion;
+}
 
 ?>
