e670f3206949378b2367c2228ff50c5f40eb7fa6
YadidSLM <yadid_sebastian@comunidad.unam.mx>
2025-07-11 03:23:33 -0600

    Ya quedó la estrella, se juntarion en un solo for el que hacía el círculo interno y el que hacía el círculo externo, se ve chueco en el número de picos 17 y 22 por el Math.ceil, es mejor usar round

--- Actividades_Tarea/dibujar_estrella/dynamics/dibuja.js
+++ Actividades_Tarea/dibujar_estrella/dynamics/dibuja.js
@@ -6,67 +6,53 @@ const canW = 300;
 const canvas = document.getElementById("estrella");
 const ctx = canvas.getContext("2d");
 
-function drawStar(lineColor, puntiagudez, numPicos){
+function drawStar(lineColor, puntiagudez, numPicos, rellenar){
     let x1 = 150;
     let y1 = 150;
     let longitud = [puntiagudez, 145];
-    let grados = 0;
     let rad;
-    let xDestino;
-    let yDestino;
-    // console.clear();
+    let xDestino = [undefined, undefined];
+    let yDestino = [undefined, undefined];
+    document.body.parentElement.style.backgroundColor = lineColor;
+    document.body.style.backgroundColor = lineColor;
     ctx.beginPath();
 
-    //Interior
+    //Estrella
     for(let theta = 0; theta < 360; theta++){
         rad = (theta) * (Math.PI/180);
-        xDestino = longitud[0] * Math.cos(rad) + x1;
-        yDestino = longitud[0] * Math.sin(rad) + y1;
-        //Dibuja circunferencia
-        // if(theta == 0){
-        //     ctx.moveTo(xDestino, yDestino);
-        // } else {
-        //     ctx.lineTo(xDestino, yDestino);
-        // }
-        //Puntos internos de la estrella
+        xDestino[0] = longitud[0] * Math.cos(rad) + x1;
+        yDestino[0] = longitud[0] * Math.sin(rad) + y1;
+        mitadRadEntrePicos = (Math.ceil(360/numPicos) / 2) * (Math.PI/180); //Ángulo en radianes de la mitad de ángulo que hay entre cada pico.
+        xDestino[1] = longitud[1] * Math.cos(rad + mitadRadEntrePicos) + x1;
+        yDestino[1] = longitud[1] * Math.sin(rad + mitadRadEntrePicos) + y1;
+
         for(let pic = 0; pic < numPicos; pic++){
             if(theta == (Math.ceil(360/numPicos)) * pic){
-                //Elipse
-                ctx.moveTo(xDestino, yDestino);
-                ctx.ellipse(xDestino, yDestino, 5, 5, 0, 0, 2 * Math.PI);
+                ctx.lineTo(xDestino[0], yDestino[0]);
+                ctx.lineTo(xDestino[1], yDestino[1]);
             }
-        }  
+            if(pic == numPicos - 1 && theta == 359){ //Si la pluma acabó de dibujar el último pico y está en la última iteración de theta entonces vuelve a la primera posición donde se empezó a dibujar el primer pico.
+                xDestino[0] = longitud[0] * Math.cos(0) + x1;
+                yDestino[0] = longitud[0] * Math.sin(0) + y1;
+                ctx.lineTo(xDestino[0], yDestino[0]);
+            }
+        }
     }
-    
-    //Exterior
-    for(let theta = 0; theta < 360; theta++){
-        rad = (theta) * (Math.PI/180);
-        mitadRadEntrePicos = (Math.ceil(360/numPicos) / 2) * (Math.PI/180); //Ángulo en radianes de la mitad de ángulo que hay entre cada pico.
-        xDestino = longitud[1] * Math.cos(rad + mitadRadEntrePicos) + x1;
-        yDestino = longitud[1] * Math.sin(rad + mitadRadEntrePicos) + y1;
-        //Dibuja circunferencia
-        // if(theta == 0){
-        //     ctx.moveTo(xDestino, yDestino);
-        // } else {
-        //     ctx.lineTo(xDestino, yDestino);
-        // }
-        //Puntos internos de la estrella
-        for(let pic = 0; pic < numPicos; pic++){
-            if(theta == ((Math.ceil(360/numPicos)) * pic)){
-                //Elipse
-                ctx.moveTo(xDestino, yDestino);
-                ctx.ellipse(xDestino, yDestino, 5, 5, 0, 0, 2 * Math.PI);
-            }
-        }  
+    if(rellenar){
+        ctx.fillStyle = lineColor;
+        ctx.fill();
+    } else {
+        ctx.fillStyle = "#ffffff";
+        ctx.fill();
     }
     ctx.strokeStyle = lineColor;
     ctx.stroke();
     ctx.closePath();
 }
 
-drawStar(star_color.value, puntiagudez.value, num_picos.value, num_picos.value);
+drawStar(star_color.value, puntiagudez.value, num_picos.value, rellenar.checked);
 
-
+//Cuando haya un evento
 num_picos.addEventListener("change", ()=>{
     if(num_picos.value > 30){
         num_picos.value = 30;
@@ -74,23 +60,20 @@ num_picos.addEventListener("change", ()=>{
         num_picos.value = 3;
     }
     ctx.clearRect(0, 0, canW, canW);
-    console.log(num_picos.value);
-    drawStar(star_color.value, puntiagudez.value, num_picos.value);
+    drawStar(star_color.value, puntiagudez.value, num_picos.value, rellenar.checked);
 });
 
-puntiagudez.addEventListener("change", ()=>{
+puntiagudez.addEventListener("input", ()=>{
     ctx.clearRect(0, 0, canW, canW);
-    console.log(puntiagudez.value);
-    drawStar(star_color.value, puntiagudez.value, num_picos.value);
+    drawStar(star_color.value, puntiagudez.value, num_picos.value, rellenar.checked);
 });
 
 star_color.addEventListener("input", ()=>{
     ctx.clearRect(0, 0, canW, canW);
-    console.log(star_color.value);
-    drawStar(star_color.value, puntiagudez.value, num_picos.value);
+    drawStar(star_color.value, puntiagudez.value, num_picos.value, rellenar.checked);
     
 });
 
 rellenar.addEventListener("change", ()=>{
-    console.log(rellenar.checked);
+    drawStar(star_color.value, puntiagudez.value, num_picos.value, rellenar.checked);
 });
